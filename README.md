9633d34bc2fdeda89ed0f1170d1de15df7e17572
rodrigo.kakiuchi <rodrigo.kakiuchi2010@gmail.com>
2024-07-01 17:03:15 -0300

    Novo README

---
+++ README.md
@@ -0,0 +1,4 @@
+# BackEnd Agenda
+
+# Informações
+As informações se encontram no [repositório do Front end](https://github.com/Rodrigokaki/Fatec-Projeto-Final-Front)
--- src/main/java/com/fatec/agenda/resources/ContactController.java
+++ src/main/java/com/fatec/agenda/resources/ContactController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.fatec.agenda.dtos.ContactRequest;
 import com.fatec.agenda.dtos.ContactResponse;
 import com.fatec.agenda.services.ContactService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("contacts")
 public class ContactController {
